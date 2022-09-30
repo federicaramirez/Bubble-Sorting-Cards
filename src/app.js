240d9de6f16d20fc7ef34deff34900a6ce6cdd26
federicaramirez <federicaaramirez@gmail.com>
2022-09-30 17:35:37 +0000

    cartasordenadas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function generarCartas() {
+    let palos = ["♦", "♥", "♠", "♣"];
+    let number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+
+    let paloaleatorio = palos[Math.floor(Math.random() * palos.length)];
+    let numeroaleatorio = number[Math.floor(Math.random() * number.length)];
+
+    document.getElementById("paloarriba").innerHTML = paloaleatorio;
+    document.getElementById("paloabajo").innerHTML = paloaleatorio;
+    document.getElementById("numerocentro").innerHTML = numeroaleatorio;
+
+    return numeroaleatorio + paloaleatorio;
+  }
+  generarCartas();
+
+  //Generar un Array con la cantidad de cartas según el imput
+
+  function generarArray() {
+    let cantidadDeCartas = document.getElementById("numero-cartas").value;
+    let arrCartas = [];
+    // arrCartas.length = cantidadDeCartas;
+    for (let i = 0; i < cantidadDeCartas; i++) {
+      let cartaAleatoria = generarCartas();
+      arrCartas.push(cartaAleatoria);
+      console.log(arrCartas);
+    }
+  }
+
+  //Btn hacer que se muestren las cartas
+  document
+    .getElementById("btncrearcartas")
+    .addEventListener("click", generarArray);
+
+  //btn que las muestre
 };
